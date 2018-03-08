6a5a6639ef1cb5ed1c276be02bf06da5f16e151b
Himanshu Prajapati <himanshuprajapati03@gmail.com>
2018-03-08 14:24:56 +0530

    Add files via upload

---
+++ HPPattern.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.lang.*;
+
+class HPPattern
+{
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter the string");
+		
+		int n = Integer.parseInt(br.readLine());
+		int m = n;
+		for(int i = 1; i <= n; i++)
+		{
+			System.out.println("");
+			int s = n - i;
+			for(int j = 1; j <= s; j++)
+			{
+				System.out.print("  ");
+			}
+
+			int x = (n - m) + 1;
+			
+			for(int j = x; j >= 1; j--)
+			{
+				System.out.print(j + " ");
+			}
+			
+			for(int j = 2; j <= x; j++)
+			{
+				System.out.print(j + " ");
+			}
+			m--;	
+		}
+		m = n;
+		for(int i = n; i >= 1; i--)
+		{
+			System.out.println("");
+			int s = m - n;
+			
+			for(int j = 0; j <= s; j++)
+			{
+				System.out.print("  ");
+			}
+			
+			int x = n - 1;
+			
+			for(int j = x; j >= 1; j--)
+			{
+				System.out.print(j + " ");
+			}
+			
+			for(int j = 2; j <= x; j++)
+			{
+				System.out.print(j + " ");
+			}
+			n--;			
+		}
+	}
+}
